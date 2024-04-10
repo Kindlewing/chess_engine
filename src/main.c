c70329edcda6011a0ddf866f549fd7218221aa78
hudson <hfinn@autodist.com>
2024-04-10 16:15:04 -0400

    Add initial project

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int NONE = 0;
+	int KING = 1;
+	int PAWN = 2;
+	int KNIGHT = 3;
+	int BISHOP = 4;
+	int ROOK = 5;
+	int QUEEN = 6;
+
+	int BLACK = 8;
+	int WHITE = 16;
+
+	int *board[64];
+	return EXIT_SUCCESS;
+}
