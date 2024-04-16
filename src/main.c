373f8427ea07f325a39f6c630239e98236985081
Hudson Finn <hudson.finn@zohomail.com>
2024-04-16 18:07:45 -0400

    Add print_bitboard function

--- src/main.c
+++ src/main.c
@@ -1,31 +1,23 @@
 #include <stdint.h>
 #include <stdio.h>
 
+void print_bitboard(uint64_t board) {
+  for (int file = 0; file < 8; file++) {
+    for (int rank = 0; rank < 8; rank++) {
+      printf(" %lu  ", (board >> (rank * 8 + file)) & 1);
+    }
+    printf("\n");
+  }
+}
+
 uint64_t set_bit(uint64_t bitboard, int rank, int file) {
-	uint64_t bit = 1L;
-	return bitboard |= (bit << (rank * 8 + file));
+  long bit = 1L << (rank * 8 + file);
+  return (bitboard | bit);
 }
 
 int main() {
-	uint64_t bitboard = 0L;
+  uint64_t bitboard = 0L;
+  print_bitboard(bitboard);
 
-	for(int file = 0; file < 8; file++) {
-		for(int rank = 0; rank < 8; rank++) {
-			printf(" %lu ", bitboard);
-		}
-		printf("\n");
-	}
-
-	printf("\n\n");
-	uint64_t new_board = set_bit(bitboard, 4, 4);
-
-	for(int file = 0; file < 8; file++) {
-		for(int rank = 0; rank < 8; rank++) {
-			printf(" %lu ", new_board);
-		}
-		printf("\n");
-	}
-	return 0;
+  return 0;
 }
-
-
