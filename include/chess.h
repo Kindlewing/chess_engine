1def605915d844b4b7b19a589d31b0187eadcf96
Hudson Finn <hudson.finn@zohomail.com>
2025-07-26 13:07:06 -0400

    Bitboard representation

--- include/chess.h
+++ include/chess.h
@@ -16,4 +16,18 @@ typedef uint64_t u64;
 typedef float f32;
 typedef double f64;
 
+u64 white_pawns;
+u64 white_knights;
+u64 white_bishops;
+u64 white_rooks;
+u64 white_queens;
+u64 white_king;
+
+u64 black_pawns;
+u64 black_knights;
+u64 black_bishops;
+u64 black_rooks;
+u64 black_queens;
+u64 black_king;
+
 #endif // CHESS_H
