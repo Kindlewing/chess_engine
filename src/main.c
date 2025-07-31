0d29516fec8c91a89412c2fdaf6cc2b216df2980
Hudson Finn <hudson.finn@zohomail.com>
2025-07-31 16:43:30 -0400

    Boiard

--- src/main.c
+++ src/main.c
@@ -1,19 +1,16 @@
 #include <stdlib.h>
 #include "chess.h"
 
-board *board_create() {
-    board *board = malloc(sizeof *board);
-
-    return board;
-}
-
 int main(void) {
+    board board = {0};
+
     string *starting_fen = string_create_from_cstring(
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", false);
 
-    board->all_pieces = 0;
-    board->all_pieces = board->all_pieces | (1ULL << g3);
-    bb_print(board->all_pieces);
+    board.all_pieces = 0;
+    board.all_pieces = board.all_pieces | (1 << g3);
+
+    bb_print(board.all_pieces);
     string_destroy(starting_fen);
     return 0;
 }
