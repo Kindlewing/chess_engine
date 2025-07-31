b507d34a1298a360028e00e6e664308203d36ffa
Hudson Finn <hudson.finn@zohomail.com>
2025-07-31 16:40:46 -0400

    Board initialization

--- src/main.c
+++ src/main.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "chess.h"
 
+board *board_create() {
+    board *board = malloc(sizeof *board);
+
+    return board;
+}
+
 int main(void) {
-    string *fen = string_create_from_cstring(
+    string *starting_fen = string_create_from_cstring(
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", false);
 
-    bitboard starting_board = bb_generate_from_fen(fen);
-    bb_print(starting_board);
-    string_destroy(fen);
+    board->all_pieces = 0;
+    board->all_pieces = board->all_pieces | (1ULL << g3);
+    bb_print(board->all_pieces);
+    string_destroy(starting_fen);
     return 0;
 }
